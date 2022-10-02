fb9f037f26e43f09308ed0d2846a6a3e2d08c174
wick3dr0se <wick3dr0se@protonmail.com>
2022-10-02 00:40:15 -0500

    add mkmod function & spellcheck fixes

--- archstrap.sh
+++ archstrap.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 
 LC_ALL=C
-input=${@//-*}
+input=${*//-*}
 
 # helper functions 
 _out() {
-  if [[ $2 == x ]] ; then
-    code=1
-    response='Err'
-  elif [[ $2 == w ]] ; then
+  if [[ $2 == e ]] ; then
     code=3
-    response='Warn'
+    msg='Error'
+  elif [[ $2 == x ]] ; then
+    code=1
+    msg='Fatal'
+  else
+    code=2
+    msg='Pass'
   fi
-
   printf '[\e[1;3%sm%s\e[0m]: %s\n' \
-    "${code:-2}" "${response:-Pass}" "$1"
-
-  [[ $code == 1 ]] && exit 1
+    "$code" "$msg" "$1"
+      
+  [[ $msg == Fatal ]] && exit 1
 }
 
 get_reply() {
   printf '[\e[32my\e[0m/\e[31mN\e[0m]: '
-  read reply
-  reply=${reply:-N}
+  read -r reply
+  reply="${reply:-N}"
 
-  [[ $reply == y || $reply == yes ]] && 
+  [[ ${reply,,} == y || ${reply,,} == yes ]] && 
     return 0 ||
     return 1
 }
 
+mkmod() {
+  local one="$1" ; shift
+
+  mkdir -p "$@" && chmod "$one" "$@"
+}
+
 # define booleans
-hostPkgs=0
+#hostPkgs=0
 hostMirrors=1
 hostKeyring=1
 
 # USAGE
 (( $# )) && {
-  for i in $@ ; do
+  for i in "$@" ; do
     case $i in
-      --hostcache|-h) hostPkgs=1 ;;
+      #--hostcache|-h) hostPkgs=1 ;;
       --no-mirrors|-m) hostMirrors=0 ;;
       --no-keyring|-k) hostKeyring=0 ;;
     esac
@@ -53,19 +61,24 @@ get_packages() {
   repo=${1:-core} && shift
   addPkgs='base'
   addPkgs+="$2"
-  repoDB="$repo.db.tar.gz"
+  repoDB="$repo".db.tar.gz
   mirror="$baseMirror/$repo/os/$arch"
 
-  # download package database
-  _out "Downloading $repo packages.."
-  curl -sLO $mirror/$repo.db.tar.gz
+  # get $repo package database
+  curl -sLO "$mirror/$repo".db.tar.gz
 
   # extract to $repo
-  [[ -d $repo ]] || mkdir $repo
-  tar xf $repoDB -C $repo 2>/dev/null && rm -fr $repoDB
+  [[ -d $repo ]] || mkdir "$repo"
+  tar -xf "$repoDB" -C "$repo" 2>/dev/null && rm -fr "$repoDB"
 
-: ' # copy host packages 
-  (( $hostPkg )) && {
+  # create a package list
+  [[ -f packages ]] && rm -fr packages
+  for i in "$repo"/* ; do
+    i="${i/$repo\/}"
+    echo "${i%%-[0-9]*}" >> packages
+  done
+
+: '(( hostPkg )) && {
     [[ -d /var/lib/pacman ]] && {
       pacman -Qe
     }
@@ -78,31 +91,31 @@ make_tmpfs() {
 
   # check if $tmpfs already exist 
   [[ -d $tmpfs ]] && {
-    _out "$tmpfs already exist. Do you want to overwrite?" w
+    _out "$tmpfs already exist. Do you want to overwrite?" e
     get_reply || _out 'Aborting..' x
-    rm -fr $tmpfs
+    rm -fr "$tmpfs"
   }
 
-  # make the root filesystem, with proper permissions
-  mkdir -m 755 -p "$tmpfs"/var/{cache/pacman/pkg,lib/pacman,log} "$tmpfs"/{dev,run,etc/pacman.d}
-  mkdir -m 777 -p "$tmpfs"/tmp
-  mkdir -m 555 -p "$tmpfs"/{sys,proc}
+# make the root filesystem, with proper permissions
+  mkmod 777 "$tmpfs"/tmp
+  mkmod 755 "$tmpfs"/var/{cache/pacman/pkg,lib/pacman,log} "$tmpfs"/{dev,run,etc/pacman.d}
+  mkmod 555 "$tmpfs"/{sys,proc}
 
   get_packages core "$input"
 
   # copy network config
-  [[ -f /etc/resolv ]] && cp /etc/resolv "$tmpfs/etc/"
+  [[ -f /etc/resolv ]] && cp /etc/resolv "$tmpfs"/etc/
   
   # copy keyring
-  (( $hostKeyring )) && [[ -d /etc/pacman.d/gnupg ]] &&
-    cp -a --no-preserve=ownership /etc/pacman.d/gnupg "$tmpfs/etc/pacman.d/"
+  (( hostKeyring )) && (( ! EUID )) && [[ -d /etc/pacman.d/gnupg ]] &&
+    cp -a --no-preserve=ownership /etc/pacman.d/gnupg "$tmpfs"/etc/pacman.d/
 
   # add global mirror
-  echo "Server = $mirror" >> "$tmpfs/etc/pacman.d/mirrorlist"
+  echo "Server = $mirror" >> "$tmpfs"/etc/pacman.d/mirrorlist
 
   # copy host mirrors
-  (( $hostMirrors )) && [[ -f /etc/pacman.d/mirrorlist ]] &&
-    cp -a /etc/pacman.d/mirrorlist "$tmpfs/etc/pacman.d/"
+  (( hostMirrors )) && [[ -f /etc/pacman.d/mirrorlist ]] &&
+    cp -a /etc/pacman.d/mirrorlist "$tmpfs"/etc/pacman.d/
 }
 
 make_tmpfs "$input"
