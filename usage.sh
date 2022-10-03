cc04c178781adccd9e5677dc2e1616adc7f8f18e
wick3dr0se <wick3dr0se@protonmail.com>
2022-10-03 14:34:26 -0500

    usage changes

--- usage.sh
+++ usage.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 # usage.sh
 
-(( $# )) && {
-  for i in "$@" ; do
-    case $i in
-      -?(-)[a-z]?([a-z])*) [[ $i =~ ^-- ]] || {
-          while read -rn1 flag ; do
-            flags+=" -${flag}"
-          done <<< ${i#-}
-        }
-        flags="${flags:-$i}"
-        for i in ${flags%-} ; do
-          flag=$i
-          case $flag in
-            #--hostcache|-h) hostCache=1 ;; # add host packages
-            --no-mirrors|-m) hostKeyring=0 ;; # copy host pacman keyring
-            --no-keyring|-k) hostMirrors=0 ;; # copy host mirrorlist
-            *) _out 'Bad argument' x ;;
-          esac
-        done
-      ;;
-    esac
-  done
-}
+# cuz fuck getopts
+for i in "$*" ; do
+  [[ $i =~ ^-(-)?[a-z] ]] && {
+    [[ $i =~ ^-[a-z] ]] && {
+      while read -rn1 flag ; do
+        flags+=" -${flag}"
+      done <<< ${i#-}
+    }
+    flags="${flags:-$i}"
+    
+    for i in ${flags%-} ; do
+      flag=$i
+      case $flag in
+        #--hostcache|-c) hostCache=1 ;; # add host packages
+        --no-mirrors|-m) hostKeyring=0 ;; # copy host pacman keyring
+        --no-keyring|-k) hostMirrors=0 ;; # copy host mirrorlist
+        *) _out 'Bad argument' x ;;
+      esac
+    done
+  }
+done
