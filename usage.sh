a8f7d5bfafb16bb5f915a78bf1a37759326fed35
wick3dr0se <wick3dr0se@protonmail.com>
2022-10-04 22:12:46 -0500

    refactor, add usage, disable shellchecks

--- usage.sh
+++ usage.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 # usage.sh
 
-# cuz fuck getopts
-for i in "$*" ; do
-  [[ $i =~ ^-(-)?[a-z] ]] && {
-    [[ $i =~ ^-[a-z] ]] && {
-      while read -rn1 flag ; do
-        flags+=" -${flag}"
-      done <<< ${i#-}
-    }
-    flags="${flags:-$i}"
-    
-    for i in ${flags%-} ; do
-      flag=$i
-      case $flag in
-        #--hostcache|-c) hostCache=1 ;; # add host packages
-        --no-mirrors|-m) hostKeyring=0 ;; # copy host pacman keyring
-        --no-keyring|-k) hostMirrors=0 ;; # copy host mirrorlist
-        *) _out 'Bad argument' x ;;
-      esac
-    done
+_usage() {
+cat <<-USAGE
+ usage | archstrap <newroot> <packages>
+-----------------------------------------
+ -c,--hostcache | install host packages to newroot
+ -k, --no-keyring | don't copy host pacman keyring to newroot
+ -m, --no-mirrors | don't copy host mirrorlist to newroot
+USAGE
+exit
+}
+
+get_flags() { # bcuz fuck getopts
+for i in $inputFlags ; do
+  [[ $i =~ ^-[a-z] ]] && {
+    while read -rn1 flag ; do
+      flags+=" -${flag#-}"
+    done <<< $i
   }
+  flags=${flags:-$i}
+
+  for i in ${flags//-} ; do
+    case $i in
+      h|u|help|usage) _usage ;; # get usage
+      #c|hostcache) hostCache=1 ;; # add host packages
+      k|no-keyring) hostKeyring=0 ;; # don't copy host pacman keyring
+      m|no-mirrors) hostMirrors=0 ;; # don't copy host mirrorlist
+      *) _out 'Bad argument' x ;;
+    esac
+  done
 done
+}
