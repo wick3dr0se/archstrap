97a1e5da08703a9d282e7748ea0b737c0cf77b0e
wick3dr0se <wick3dr0se@protonmail.com>
2023-04-14 01:20:35 -0700

    drop _rm function definition

--- helpers.sh
+++ helpers.sh
@@ -10,5 +10,3 @@ _msg(){ printf 'archstrap [\e[1;32m🗸\e[m]: %s\n' "$1"; }
 _warn(){ printf 'archstrap [\e[1;33m❗\e[m]: %s\n' "$1"; }
 
 _err(){ printf 'archstrap [\e[1;31m❌\e[m]: %s\n' "$1"; exit 1; }
-
-_rm(){ rm -r "${@:?}"; }
