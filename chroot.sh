35a87be49a39d7d766647948dfa87e7afcd85dac
wick3dr0se <wick3dr0se@protonmail.com>
2023-04-14 02:00:52 -0700

    refactor; removed all helper functions

--- chroot.sh
+++ chroot.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-. helpers.sh
-
 mnt="$1"; shift
-packages=( 'dosfstools' 'ntfs-3g' 'parted' 'gdisk' "$@" ); _div
+packages=( 'dosfstools' 'ntfs-3g' 'parted' 'gdisk' "$@" )
 
 systemd-machine-id-setup
 
-pacman-key --init; pacman-key --populate; _div
+pacman-key --init; pacman-key --populate
+pacman -Syu --needed --noconfirm "${packages[@]}"&& _msg 'Updated & installed packages'
 
-pacman -Syu --needed --noconfirm "${packages[@]}"&& _msg 'Updated & installed packages'; _div
-
-_msg "Done. Arch Installer environment setup; Changed root into $mnt/root.x86_64"
+printf '\e[32m>\e[m: %s\n' "Done. Arch installer environment setup; Chaged root into $mnt/root.x86_64"
 printf 'You may now proceed to: https://wiki.archlinux.org/title/Installation_guide#Partition_the_disks and follow the rest of the installation guide\n'
 
 printf '%s\n' 'echo "Welcome to Arch Linux"'>> /etc/profile
