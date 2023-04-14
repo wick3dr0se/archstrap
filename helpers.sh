99f6860a5064883488929abfff2175b6c160b78b
wick3dr0se <wick3dr0se@protonmail.com>
2023-04-13 23:06:24 -0700

    remove unecessary _curl function

--- helpers.sh
+++ helpers.sh
@@ -16,5 +16,3 @@ _rm(){ rm -r "${@:?}"; }
 _untar(){ tar xzf "$1" -C "${1%/*}" --numeric-owner&& _rm "$@"; }
 
 _unmount(){ findmnt -M "$1"&& umount "$1"; :; }
-
-_curl(){ for _; do curl "$_" > "$mnt/${_##*/}"; done; }
