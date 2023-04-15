f3a2ec277f8909d449eaff164905fc7f63d483c5
wick3dr0se <wick3dr0se@protonmail.com>
2023-04-15 02:28:03 -0400

    correct package handling

--- chroot.sh
+++ chroot.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-packages=('dosfstools' 'ntfs-3g' 'parted' 'gdisk' "${@/$1}")
-
+mnt="$1"; shift
 systemd-machine-id-setup
 
 pacman-key --init; pacman-key --populate
-pacman -Syu --needed --noconfirm "${packages[@]}"&& printf '\e[32m>\e[m: %s\n' 'Updated & installed packages'
+pacman -Syu --needed --noconfirm "$@"&& printf '\e[32m>\e[m: %s\n' 'Updated & installed packages'
 
-printf '\e[32m>\e[m: %s\n%s\n' "Done. Arch installer environment setup; Chaged root into $1/root.x86_64" \
+printf '\e[32m>\e[m: %s\n%s\n' "Done. Arch installer environment setup; Chaged root into $mnt/root.x86_64" \
   'You may now proceed to: https://wiki.archlinux.org/title/Installation_guide#Partition_the_disks and follow the rest of the installation guide'
 
 printf '%s\n' 'echo "Welcome to Arch Linux"'>> /etc/profile
