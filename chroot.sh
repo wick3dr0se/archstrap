e6b9059ec3a3f354c2e9af286a96d0119520e544
wick3dr0se <wick3dr0se@protonmail.com>
2023-10-24 15:34:11 +0000

    refactor

--- chroot.sh
+++ chroot.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-mnt="$1"; shift
+rootFS="$1"; shift
 systemd-machine-id-setup
 
-pacman-key --init; pacman-key --populate
+pacman-key --init
+pacman-key --populate
 pacman -Syu --needed --noconfirm "$@"&& printf '\e[32m>\e[m: %s\n' 'Updated & installed packages'
 
-printf '\e[32m>\e[m: %s\n%s\n' "Done. Arch installer environment setup; Chaged root into $mnt/root.x86_64" \
+printf '\e[32m>\e[m: %s\n%s\n' "Done. Arch installer environment setup; Chaged root into $rootFS" \
   'You may now proceed to: https://wiki.archlinux.org/title/Installation_guide#Partition_the_disks and follow the rest of the installation guide'
 
-printf '%s\n' 'echo "Welcome to Arch Linux"'>> /etc/profile
+printf '%s\n' 'echo "Welcome to Arch Linux"' >>/etc/profile
 bash --login
